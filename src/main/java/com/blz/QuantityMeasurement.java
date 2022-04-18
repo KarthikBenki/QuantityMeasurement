aa42023f35223df62c10ca7eda6fdc96af869ac3
KarthikBenki <karthikmc007@gmail.com>
2022-04-18 15:07:18 +0530

    Added:comparision of units

---
+++ src/main/java/com/blz/QuantityMeasurement.java
@@ -0,0 +1,9 @@
+package com.blz;
+
+public class QuantityMeasurement {
+
+  public boolean compareUnits(double feet, double inch) {
+    if (inch == 12 * feet) return true;
+    return false;
+  }
+}
